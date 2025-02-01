2144f96340cd6098127c0a97e378c2fbe0bcccad
KBandipo~ <keneebandipo@gmail.com>
2025-02-01 21:54:59 +0100

    feat: Add Button style

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import './Button.css'
+
 interface ButtonProps { title: string }
 export default function Button({ title }: ButtonProps) {
   return (
-    <button>{title}</button>
+    <button className="UI-Button">{title}</button>
   )
 }
